8e07145ee8cc29770e80450629b0352090488448
jinting92 <544965448@qq.com>
2019-01-05 20:34:24 +0800

    small

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 class Dashboard extends React.Component {
     render() {
         return (
-            <div className="gutter-example button-demo">12
+            <div className="gutter-example button-demo">1
             </div>
         )
     }
