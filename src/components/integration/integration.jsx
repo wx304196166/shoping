5d69ea36be085ad7d1c013aad469354e7ca55bc3
wx304196166 <13327468696@163.com>
2019-01-19 18:23:48 +0800

    更新

--- src/components/integration/integration.jsx
+++ src/components/integration/integration.jsx
@@ -1,16 +1,36 @@
 /**
- * Created by katniss on 2017/5/3.
+ * Created by katniss on 2019/1/13.
  */
 import React from 'react';
+import Related from '../common/releated/related';
+import './integration.scss';
 
 class Integration extends React.Component {
-    render() {
-        return (
-            <div className="page-container">
-                integration
-            </div>
-        )
-    }
+  render() {
+    return (
+      <ul className="page-container clearfix">
+        <li className="show-box clearfix">
+          <ul className="show clearfix">
+            <li>Led Colourful HD Display</li>
+            <li></li>
+            <li>Specifications:  <span>200*300</span></li>
+            <li>
+            Curabitur auctor tristique lobortis. Quisque bibendum, ipsum in feugiat pharetra, odio libero malesuada turpis, tempus fermentum augue est sit amet magna. Vestibulum bibendum lectus non mauris porta, sed blandit purus scelerisque. Sed consequat mollis ornare. Sed laoreet id dolor vitae facilisis. Mauris varius orci sed turpis commodo mattis.
+            </li>
+          </ul>
+          <div className="blank"></div>
+          <ul className="operate clearfix">
+            <li></li>
+            <li></li>
+          </ul>
+          <div className="blank"></div>
+          <div className="related"><Related /></div>
+        </li>
+        <li className="parms-table"></li>
+        <li className="submit"></li>
+      </ul>
+    )
+  }
 }
 
 export default Integration;
